68214815df3467ccc8dfc1d16a6a68ca08741446
Patrick Harken <2151687+pharken@users.noreply.github.com>
2023-08-21 11:02:59 -0500

    url2section value upgrades

--- src/main/webapp/js/misc/urlToSectionValues.js
+++ src/main/webapp/js/misc/urlToSectionValues.js
@@ -1,11 +1,29 @@
+/**
+
+ Rules for naming convention syntax
+
+ ---Name---------------------Syntax-------------------------------------------Example-------------------------------
+ Campaign				LOB-division-language								Tracfone-Care-Spa
+ Enagement Name 		<<BASE>>(_language)_entryPointType_behavior1-N		TF_sales_accessories_audio_es_S_AO-30s
+ Engagement Window		LobInitials-division(-language)-uniqueIdentifier	TF-sales-es-accessories-all-es
+ Entry Point			<<BASE>>(-language)									TF-sales-accessories-audio-es
+ Behavior				<<BASE>>(-language)-behaviors1-n					TF-sales-accessories-audio-AO-30s
+ skill					LobInitials_division+Language 						TF_SalesSpa
+
+ language only when not the default - english
+
+ */
+
 let textarea = null;
 const result = document.getElementById("url2sectionsResultSection");
 const SPANISH_IDENTIFIER = 'es';
-let entryPointPrefix = 'TF-';
+let lobPrefix = 'TF';
 let salesOrCare = 'sales';
+let entryPointType = 'sticky';
 let includeSubdomain = true;
 let includeSpanish = true;
-
+let behaviorAutoOpen = false;
+let behaviorTimeOnPage = false;
 
 
 const init = function () {
@@ -21,17 +39,30 @@ const init = function () {
     const toEngagementsBtn = document.getElementById("url2engagementsBtn");
     toEngagementsBtn.addEventListener("click", convertUrlToEngagements);
 
+    const toBehaviorsBtn = document.getElementById("url2behaviorsBtn");
+    toEngagementsBtn.addEventListener("click", convertUrlToEngagements);
+//TODO
+    const toEngagementWindowsBtn = document.getElementById("url2EngagementWindowBtn");
+    toEngagementsBtn.addEventListener("click", convertUrlToEngagements);
+
     const clearBtn = document.getElementById("clearBtn");
     clearBtn.addEventListener("click", clearResults);
 
     bindLobDropdownBtn();
     bindSaleOrCareDropdownBtn();
+    bindEntryPointTypeDropdownBtn();
 
     $('#includeSubdomainCheckbox').change(function() {
         includeSubdomain = $(this).prop('checked');
     })
     $('#includeSpanishCheckbox').change(function() {
         includeSpanish = $(this).prop('checked');
+    })
+    $('#behaviorAutoOpen').change(function() {
+        behaviorAutoOpen = $(this).prop('checked');
+    })
+    $('#behaviorTimeOnPage').change(function() {
+        behaviorTimeOnPage = $(this).prop('checked');
     })
 }
 
@@ -40,13 +71,12 @@ const bindLobDropdownBtn = function (){
     let $lobDropDownBtn = $('#lobDropDownBtn');
     $lobDropDownBtn.html('TF <span class="caret"></span>');
     $('#lobDropDownMenu').find("li a").click( function(){
-        let selText = $(this).text();
-        $lobDropDownBtn.html(selText + ' <span class="caret"></span>');
-        entryPointPrefix = selText.concat('-');
+        let entryPointPrefix = $(this).text();
+        $lobDropDownBtn.html(entryPointPrefix + ' <span class="caret"></span>');
         // console.log(`entry point prefix: ${entryPointPrefix}`);
     });
+    //$lobDropDownBtn.trigger('click');
 }
-
 
 
 const bindSaleOrCareDropdownBtn = function (){
@@ -59,6 +89,16 @@ const bindSaleOrCareDropdownBtn = function (){
     });
 }
 
+
+const bindEntryPointTypeDropdownBtn = function (){
+    let entryPointTypeBtn = $('#entryPointTypeDropdownBtn');
+    entryPointTypeBtn.html('sticky <span class="caret"></span>');
+    $('#entryPointTypeDropdownMenu').find("li a").click( function(){
+        entryPointType = $(this).text();
+        entryPointTypeBtn.html(entryPointType + ' <span class="caret"></span>');
+        // console.log(`sales or care: ${salesOrCare}`);
+    });
+}
 
 
 
@@ -163,50 +203,123 @@ const splitIntoSections = function (row){
 }
 
 
-const createEntryPointNames = function (row){
-    let newRow = entryPointPrefix;
+/*
+Naming convention base name
+
+LobInitials  +  division  +  (language)  +  urlPathLevel-1 to urlPathLevel-n
+
+Add language when not default - English
+*/
+const createNamingConventionBaseName = function (row, delimiter){
+    // new row starts with the LOB prefix
+    let newRow = lobPrefix.concat(delimiter);
     let sections = row.split('\/');
 
+    // remove first array element
     if (!includeSubdomain)
         sections.shift();
 
+    // check for remove spanish
     if (!includeSpanish) {
         const index = sections.indexOf(SPANISH_IDENTIFIER);
         if (index > -1)
             sections.splice(index, 1);
     }
 
+    // add the LOB division - insert into the beginning of the array
+    if (salesOrCare !== 'none')
+        sections.unshift(salesOrCare);
+
+    for (let elem of sections) {
+        newRow = newRow.concat( `${elem}${delimiter}` );
+    }
+
+    return newRow
+}
+
+
+/*
+ ---Name---------------------Syntax-------------------------------------------Example-------------------------------
+ Entry Point			<<BASE>>(-language)									TF-sales-accessories-audio-es
+*/
+const createEntryPointNames = function (row){
+/*
+    // new row starts with the LOB prefix
+    let newRow = lobPrefix.concat('-');
+    let sections = row.split('\/');
+
+    // remove first array element
+    if (!includeSubdomain)
+        sections.shift();
+
+    // check for remove spanish
+    if (!includeSpanish) {
+        const index = sections.indexOf(SPANISH_IDENTIFIER);
+        if (index > -1)
+            sections.splice(index, 1);
+    }
+
+    // add the LOB division - insert into the beginning of the array
     if (salesOrCare !== 'none')
         sections.unshift(salesOrCare);
 
     for (let elem of sections) {
         newRow = newRow.concat( `${elem}-` );
     }
+*/
+    let newRow = createNamingConventionBaseName(row, '-');
+
     return newRow.substring(0, newRow.length - 1); // remove last dash
 }
 
 
+
+
+/* ---Name---------------------Syntax-------------------------------------------Example-------------------------------
+ Enagement Name 		<<BASE>>(_language)_entryPointType_behavior1-N		TF_sales_accessories_audio_es_S_AO-30s
+*/
 const createEngagementNames = function (row){
-    let newRow = entryPointPrefix;
-    let sections = row.split('\/');
+    let baseName = createNamingConventionBaseName(row, '_');
+    let newRow = baseName.concat( getEntryPointType('_') );
+    newRow = newRow.concat( getBehaviors() );
 
-    if (!includeSubdomain)
-        sections.shift();
-
-    if (!includeSpanish) {
-        const index = sections.indexOf(SPANISH_IDENTIFIER);
-        if (index > -1)
-            sections.splice(index, 1);
-    }
-
-    if (salesOrCare !== 'none')
-        sections.unshift(salesOrCare);
-
-    for (let elem of sections) {
-        newRow = newRow.concat( `${elem}_` );
-    }
     return newRow.substring(0, newRow.length - 1); // remove last underscore
 }
+
+
+const getEntryPointType = function (delimiter = '-') {
+    let entryPointExtension = '';
+    switch (entryPointType) {
+        case 'sticky':
+            entryPointExtension = `S${delimiter}`; break;
+        case 'overlay':
+            entryPointExtension = `O${delimiter}`; break;
+        case 'embedded':
+            entryPointExtension = `E${delimiter}`; break;
+        default:
+            console.log('No match for entry point type');
+    }
+
+
+    return entryPointExtension;
+}
+
+
+const getBehaviors = function (){
+    let behaviors = '';
+    if (behaviorAutoOpen) {
+        if (behaviorTimeOnPage)
+            behaviors = 'AO-30s_';
+        else
+            behaviors = 'AO_';
+    }
+    else {
+        if (behaviorTimeOnPage)
+            behaviors = '30s_';
+    }
+    return behaviors;
+}
+
 
 const clearResults = function () {
     result.innerHTML = '';

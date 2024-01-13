038041b3dfe1961d077023ecd8d45dd45bd2acea
Patrick Harken <2151687+pharken@users.noreply.github.com>
2024-01-12 23:34:58 -0600

    Added projects page

--- src/main/webapp/js/project/project.js
+++ src/main/webapp/js/project/project.js
@@ -1,47 +1,112 @@
 
+const loadSectionValues = function(urlParams){
+    const params = urlParams.entries();
+    for(const entry of params) {
+        if ( entry[0].startsWith('sect') ) {
+            console.log(`${entry[0]}: ${entry[1]}`);
+            lpTag.section.push( entry[1] );
+        }
+    }
+    setTimeout(() => refreshLpTag(), 2000);
+}
 
 
-
-const tableColumns = [ { title: 'Site' },{ title: 'Site ID' },{ title: 'Engagement' },{ title: 'Sections' },{ title: 'Desc' } ];
-const projectDataset = [
-    ["Playground", "49985427", "---", [ "routing", "parkinglot" ], "not sure, various playground demos" ],
-    ["VZ-QA", "87604225", "LP1testForParkingLot", [ "vzqaparkinglot" ], "PLB test, campaign: VZ TAG CS" ],
-    ["VZ-Prod", "23979466", "LP_Parking_lot_Test", [ "vzprodparkinglot" ], "PLB test, campaign: vzstore" ],
-    ["VZ-Prod", "23979466", "LP_Emergency_RSA_Satellite_Test", [ "rsa-bot", "vzprod" ], "Emergency RSA satellite test, campaign: vzstore" ],
-    ["VZ-Tracfone", "91614185", "LP_Parking_Lot_Test", [ "vz-tracfone-prod-plb-test" ], "PLB test" ],
-    ["VZ-Tracfone", "91614185", "---",   [ "lp-test", "lp-generic" ], "LP generic test" ],
-    ["VZ-Alpha", "6841549", "---",   [ "lp-plb-test", "bot-agent" ], "PLB/Afiniti. Bot agent interact with PLB" ],
-    ["VZ-Alpha", "50499881", "---",  [ "lp-plb-test", "bot-agent" ], "PLB/Afiniti. Bot agent interact with PLB" ],
-    ["VZ-Alpha", "50499881", "firstSkill",  [ "lp-plb-test", "human-agent", "firstskill" ],  "PLB/Afiniti. Human agent interact with PLB" ],
-    ["VZ-Alpha", "50499881", "secondSkill", [ "lp-plb-test", "human-agent", "secondskill" ], "PLB/Afiniti. Human agent interact with PLB"],
-    ["VZ-Alpha", "50499881", "skill0000",   [ "lp-plb-test", "human-agent", "skill0000" ],   "PLB/Afiniti. Human agent interact with PLB"],
-    ["VZ-Alpha", "50499881", "skill0001",   [ "lp-plb-test", "human-agent", "skill0001" ],   "PLB/Afiniti. Human agent interact with PLB"]
-]
-
-
-
-
-let projectMain = function (){
-
-    new DataTable('#projectTable', {
-        columns: tableColumns,
-        data: projectDataset
-    });
-
-    $('#projectTable tbody tr').on("click", function (){
-        let rowSiteName = $(this).find("td").eq(0).text();
-        console.log(rowSiteName);
-    });
-
-
+const bindCopyVisitorIdBtn = function (){
     let $copyVisitorIdBtn = $('#copyVisitorIdBtn');
     $copyVisitorIdBtn.on( "click", function() {
         copyToClipboard('visitorId');
     });
-};
+}
 
 
+const bindLpEvents = function () {
+    bindLpEvent('lp_sdes', 'VAR_ADDED', sdeAddedCallback);
+    // None of the others work unless this one is active
+    bindLpEvent('MONITORING_STATE', 'lp_SMT', monitoringStateCallback);
+    bindLpEvent('state', 'lpUnifiedWindow', stateCallback);
+    bindLpEvent('conversationInfo', 'lpUnifiedWindow', conversationInfoCallback);
+    bindLpEvent('engagementData', 'lpUnifiedWindow', engagementDataCallback);
+    bindLpEvent('maximized', 'lpUnifiedWindow', maximizedCallback);
+    bindLpEvent('minimized', 'lpUnifiedWindow', minimizedCallback);
+    // bindLpEvent('START', 'LP_OFFERS', startCallback);
+    // bindLpEvent('OFFER_DISPLAY', 'LP_OFFERS', offerDisplayCallback);
+    // bindLpEvent('OFFER_IMPRESSION', 'LP_OFFERS', offerImpressionCallback);
+    // bindLpEvent('OFFER_CLICK', 'LP_OFFERS', offerClickCallback);
+    // bindLpEvent('OFFER_REMOVE', 'LP_OFFERS', offerRemoveCallback);
+}
 
+/**
+ *
+ * @param eventName         string
+ * @param appName           string
+ * @param callbackFunc      function
+ * @param callbackFunctionExecutionContext     The object which is the execution context of the function.
+ *                                              Useful if you rely on context ("this") in your code.
+ *                                              <optional>
+ * @param async             boolean             <optional>
+ * @param triggerOnce       boolean             <optional>
+ */
+const bindLpEvent = function (eventName, appName, callbackFunc, context=null, async=true, triggerOnce=false) {
+
+    lpTag.events.bind({
+        eventName:   eventName,
+        appName:     appName,
+        func:        callbackFunc,
+        context:     context,
+        async:       async,
+        triggerOnce: triggerOnce
+    })
+}
+
+const sdeAddedCallback = ( (data) => genericBindEventCallback('lp_sdes', data));
+const monitoringStateCallback = ( (data) => genericBindEventCallback('MONITORING_STATE', data));
+const stateCallback = ( (data) => genericBindEventCallback('state', data));
+const conversationInfoCallback = ( (data) => genericBindEventCallback('conversationInfo', data));
+const engagementDataCallback = ( (data) => genericBindEventCallback('engagementData', data));
+const maximizedCallback = ( (data) => genericBindEventCallback('maximized', data));
+const minimizedCallback = ( (data) => genericBindEventCallback('minimized', data));
+const startCallback = ( (data) => genericBindEventCallback('START', data));
+const offerDisplayCallback = ( (data) => genericBindEventCallback('OFFER_DISPLAY', data));
+const offerImpressionCallback = ( (data) => genericBindEventCallback('OFFER_IMPRESSION', data));
+const offerClickCallback = ( (data) => genericBindEventCallback('OFFER_CLICK', data));
+const offerRemoveCallback = ( (data) => genericBindEventCallback('OFFER_REMOVE', data));
+
+const genericBindEventCallback = function (eventName, data) {
+    //displayInfo(`( ${getFormattedTimeStamp()} )  ${eventName}:\n ${JSON.stringify(data, null, 2)}`);
+    displayInfo(`${eventName}:\n ${JSON.stringify(data, null, 2)}`);
+
+    checkEventCallbackValueForSpecificData(data);
+}
+
+const checkEventCallbackValueForSpecificData = function (data){
+    updateText("visitorId", data);
+    updateText("agentId", data);
+    updateText("agentName", data);
+    updateText("skill", data);
+    updateText("campaignId", data);
+    updateText("conversationId", data);
+    updateText("state", data);
+}
+
+
+const updateText = function (id, obj) {
+    if (id in obj) {
+        let value = obj[id];
+        if (value) {
+            // this check exists because there can be more than one value for visitor Id
+            let $targetId = $('#' + id);
+            if (id === "visitorId") {
+                if (value.length > 30) {
+                    $targetId.html(value);
+                    if ($('#copyVisitorIdBtn').hasClass('display-none'))
+                        $('#copyVisitorIdBtn').removeClass('display-none');
+                }
+            }
+            else
+                $targetId.html(value);
+        }
+    }
+}
 
 
 const copyToClipboard = function (id) {
@@ -56,74 +121,21 @@ const copyToClipboard = function (id) {
 }
 
 
-
-
 $(function() {
-/*
-    let p = location.pathname;
-    let pageName = p.substring(p.lastIndexOf('/')+1, p.lastIndexOf('.html'));
+    console.log( "Test page begin" );
 
-    let pageSections = sections[pageName];
-    if (pageSections)
-        lpTag.section = pageSections;
-    else
-        lpTag.section = [];
+    const urlStr = window.location.search;
+    const urlParams = new URLSearchParams(urlStr);
 
-    console.log(`${pageName}: ${JSON.stringify(pageSections)}`);
+    waitForLpTagPromise.then(
+        result => loadSectionValues(urlParams),
+        error => console.log('LP tag not loading')
+    );
 
-*/
+    const pagetitle = urlParams.get('pagetitle')
+    document.title = pagetitle;
 
-    // routing is for the top right 'banking' entry point
-    // parkinglot is for the top left 'parking lot' entry point. This is a shortcut to go directly to the parking lot bot
-
-    // lpTag.section = [ "routing", "parkinglot" ];   // playground tests
-
-    /*
-    Parking Lot Test:  ( 87604225 )  Verizon - QA
-        Campaign:    VZ TAG CS
-        Engagement:  LP1testForParkingLot
-        Entry point: TestParkingLot
-    */
-    // lpTag.section = [ "vzqaparkinglot" ];
-
-    /*
-    Parking Lot Test | Verizon PROD |: 23979466
-        Campaign:    vzstore
-        Engagement:  LP_Parking_lot_Test
-        Entry point: TestParkingLot  |  sections:
-    */
-    // lpTag.section = [ "vzprodparkinglot" ];
-
-
-    /*
-    Parking Lot Test | Verizon Tracfone |: 91614185
-        Engagement:  LP_Parking_Lot_Test
-    */
-    // lpTag.section = [ "vz-tracfone-prod-plb-test" ];   // parking lot bot test (tracfone)
-    // lpTag.section = [ "lp-test", "lp-generic" ];       // LP generic test (tracfone)
-
-
-    /*
-    Playground tests - not sure what this is testing though - probably old stuff
-    */
-    // lpTag.section = [ "l1:wireline", "l2:home", "l3:internet", "l4:acp" ];   // playground tests
-
-    /*
-    Parking Lot test with Afiniti  ( 50499881, 6841549 - same for both sites )
-    Bot agent setup to interact with Parking Lot Bot
-    */
-    // lpTag.section = [ "lp-plb-test", "bot-agent" ];
-
-    /*
-    Parking Lot test with Affiniti  ( 50499881 )
-    Human agents setup to interact with Parking Lot bot
-    */
-    // lpTag.section = [ "lp-plb-test", "human-agent", "firstskill" ];
-    // lpTag.section = [ "lp-plb-test", "human-agent", "secondskill" ];
-    // lpTag.section = [ "lp-plb-test", "human-agent", "skill0000" ];
-    // lpTag.section = [ "lp-plb-test", "human-agent", "skill0001" ];
-
-    detectLpTagReady();
-    projectMain();
+    bindCopyVisitorIdBtn();
+    bindLpEvents();
 });
 
---
+++ src/main/webapp/js/project/projects.js
@@ -0,0 +1,155 @@
+
+
+const localDomain = "http://localhost:9000/view";
+const githubDomain = "https://pharken.github.io/src/main/webapp/view";
+
+
+
+const tableColumns = [ { title: 'Site' },{ title: 'Site ID' },{ title: 'Engagement' },{ title: 'Sections' },{ title: 'Desc' },{ title: 'Launch' } ];
+
+const projectDataset = [
+    ["sandbox", "49985427", "---", [ "routing", "parkinglot" ], "not sure, various playground demos" ],
+    ["VZ-QA", "87604225", "LP1testForParkingLot", [ "vzqaparkinglot" ], "PLB test, campaign: VZ TAG CS" ],
+    ["VZ-Prod", "23979466", "LP_Parking_lot_Test", [ "vzprodparkinglot" ], "PLB test, campaign: vzstore" ],
+    ["VZ-Prod", "23979466", "LP_Emergency_RSA_Satellite_Test", [ "rsa-bot", "vzprod" ], "Emergency RSA satellite test, campaign: vzstore" ],
+    ["VZ-Tracfone", "91614185", "LP_Parking_Lot_Test", [ "vz-tracfone-prod-plb-test" ], "PLB test"],
+    ["VZ-Tracfone", "91614185", "---",   [ "lp-test", "lp-generic" ], "LP generic test"],
+    ["VZ-Alpha-2", "6841549", "---",   [ "lp-plb-test", "bot-agent" ], "PLB/Afiniti. Bot agent interact with PLB" ],
+    ["VZ-Alpha", "50499881", "---",  [ "lp-plb-test", "bot-agent" ], "PLB/Afiniti. Bot agent interact with PLB" ],
+    ["VZ-Alpha", "50499881", "firstSkill",  [ "lp-plb-test", "human-agent", "firstskill" ],  "PLB/Afiniti. Human agent interact with PLB" ],
+    ["VZ-Alpha", "50499881", "secondSkill", [ "lp-plb-test", "human-agent", "secondskill" ], "PLB/Afiniti. Human agent interact with PLB" ],
+    ["VZ-Alpha", "50499881", "skill0000",   [ "lp-plb-test", "human-agent", "skill0000" ],   "PLB/Afiniti. Human agent interact with PLB" ],
+    ["VZ-Alpha", "50499881", "skill0001",   [ "lp-plb-test", "human-agent", "skill0001" ],   "PLB/Afiniti. Human agent interact with PLB" ]
+]
+
+
+let projectMain = function (){
+
+    let hrefDomain = localDomain;
+
+    let projectTable = new DataTable('#projectTable', {
+        columns: tableColumns,
+        data: projectDataset,
+        columnDefs: [
+            {
+                data: null,
+                defaultContent: '<button class="btn btn-secondary" type = "button">go</button>',
+                targets: -1
+            }
+        ]
+    });
+
+    // table row click
+/*
+    $('#projectTable tbody tr').on("click", function (){
+        let rowSiteName = $(this).find("td").eq(0).text();
+        console.log(rowSiteName);
+    });
+*/
+
+    // bind table row button click
+    projectTable.on('click', 'button', function (e) {
+        let data = projectTable.row(e.target.closest('tr')).data();
+
+        let hrefParams = '';
+        let sectionValues = data[3];
+        sectionValues.forEach( (param,i) => hrefParams+= `sect${i}=${param}&` );
+        // hrefParams = hrefParams.slice(0, -1);    // remove last &
+        hrefParams += `pagetitle=${data[0]}`;
+
+        let href = `${hrefDomain}/project/${data[0]}.html?${hrefParams}`;
+        window.location=href;
+    });
+
+    let $copyVisitorIdBtn = $('#copyVisitorIdBtn');
+    $copyVisitorIdBtn.on( "click", function() {
+        copyToClipboard('visitorId');
+    });
+};
+
+
+
+
+
+const copyToClipboard = function (id) {
+    const textToCopy = $('#' + id).text();
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            console.log("copied to clipboard: " + textToCopy);
+        })
+        .catch((error) => {
+            console.error("Failed to copy text to clipboard:", error);
+        });
+}
+
+
+
+
+$(function() {
+/*
+    let p = location.pathname;
+    let pageName = p.substring(p.lastIndexOf('/')+1, p.lastIndexOf('.html'));
+
+    let pageSections = sections[pageName];
+    if (pageSections)
+        lpTag.section = pageSections;
+    else
+        lpTag.section = [];
+
+    console.log(`${pageName}: ${JSON.stringify(pageSections)}`);
+
+*/
+
+    // routing is for the top right 'banking' entry point
+    // parkinglot is for the top left 'parking lot' entry point. This is a shortcut to go directly to the parking lot bot
+
+    // lpTag.section = [ "routing", "parkinglot" ];   // playground tests
+
+    /*
+    Parking Lot Test:  ( 87604225 )  Verizon - QA
+        Campaign:    VZ TAG CS
+        Engagement:  LP1testForParkingLot
+        Entry point: TestParkingLot
+    */
+    // lpTag.section = [ "vzqaparkinglot" ];
+
+    /*
+    Parking Lot Test | Verizon PROD |: 23979466
+        Campaign:    vzstore
+        Engagement:  LP_Parking_lot_Test
+        Entry point: TestParkingLot  |  sections:
+    */
+    // lpTag.section = [ "vzprodparkinglot" ];
+
+
+    /*
+    Parking Lot Test | Verizon Tracfone |: 91614185
+        Engagement:  LP_Parking_Lot_Test
+    */
+    // lpTag.section = [ "vz-tracfone-prod-plb-test" ];   // parking lot bot test (tracfone)
+    // lpTag.section = [ "lp-test", "lp-generic" ];       // LP generic test (tracfone)
+
+
+    /*
+    Playground tests - not sure what this is testing though - probably old stuff
+    */
+    // lpTag.section = [ "l1:wireline", "l2:home", "l3:internet", "l4:acp" ];   // playground tests
+
+    /*
+    Parking Lot test with Afiniti  ( 50499881, 6841549 - same for both sites )
+    Bot agent setup to interact with Parking Lot Bot
+    */
+    // lpTag.section = [ "lp-plb-test", "bot-agent" ];
+
+    /*
+    Parking Lot test with Affiniti  ( 50499881 )
+    Human agents setup to interact with Parking Lot bot
+    */
+    // lpTag.section = [ "lp-plb-test", "human-agent", "firstskill" ];
+    // lpTag.section = [ "lp-plb-test", "human-agent", "secondskill" ];
+    // lpTag.section = [ "lp-plb-test", "human-agent", "skill0000" ];
+    // lpTag.section = [ "lp-plb-test", "human-agent", "skill0001" ];
+
+    projectMain();
+});
+

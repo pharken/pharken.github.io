314a0dfc3d64d6610c09836d28217ca47e30f424
Patrick Harken <2151687+pharken@users.noreply.github.com>
2024-01-16 13:48:49 -0600

    projects page fully working

--- src/main/webapp/js/project/projects.js
+++ src/main/webapp/js/project/projects.js
@@ -4,15 +4,13 @@ const localDomain = "http://localhost:9000/view";
 const githubDomain = "https://pharken.github.io/src/main/webapp/view";
 const defaultPath = '/project';
 
-
-
 const tableColumns = [ { title: 'Site' },{ title: 'Site ID' },{ title: 'Engagement' },{ title: 'Sections' },{ title: 'Desc' },{ title: 'Path' } ];
 
 const projectDataset = [
     ["sandbox", "49985427", "---", [ "routing", "parkinglot" ], "not sure, various playground demos", "/misc" ],
     ["VZ-QA", "87604225", "LP1testForParkingLot", [ "vzqaparkinglot" ], "PLB test, campaign: VZ TAG CS", "" ],
-    ["VZ-Prod", "23979466", "LP_Parking_lot_Test", [ "vzprodparkinglot" ], "PLB test, campaign: vzstore", "" ],
-    ["VZ-Prod", "23979466", "LP_Emergency_RSA_Satellite_Test", [ "rsa-bot", "vzprod" ], "Emergency RSA satellite test, campaign: vzstore", "" ],
+    ["VZ-Prod", "23979466", "LP_Parking_lot_Test", [ "vzprodparkinglot" ], "<span class='fa fa-lg fa-warning isOff'></span> PLB test, campaign: vzstore", "" ],
+    ["VZ-Prod", "23979466", "LP_Emergency_RSA_Satellite_Test", [ "rsa-bot", "vzprod" ], "<span class='fa fa-lg fa-warning isOff'></span> Emergency RSA satellite test, campaign: vzstore", "" ],
     ["VZ-Tracfone", "91614185", "LP_Parking_Lot_Test", [ "vz-tracfone-prod-plb-test" ], "PLB test", ""],
     ["VZ-Tracfone", "91614185", "---",   [ "lp-test", "lp-generic" ], "LP generic test", ""],
     ["VZ-Alpha-2", "6841549", "---",   [ "lp-plb-test", "bot-agent" ], "PLB/Afiniti. Bot agent interact with PLB", "" ],
@@ -25,9 +23,7 @@ const projectDataset = [
 ]
 
 
-let projectMain = function (){
-    hrefDomain = localDomain;
-
+const initDataTable = () => {
     let projectTable = new DataTable('#projectTable', {
         columns: tableColumns,
         data: projectDataset,
@@ -40,13 +36,13 @@ let projectMain = function (){
         ]
     });
 
-/*
-    // table row click
-    $('#projectTable tbody tr').on("click", function (){
-        let rowSiteName = $(this).find("td").eq(0).text();
-        console.log(rowSiteName);
-    });
-*/
+    /*
+        // table row click
+        $('#projectTable tbody tr').on("click", function (){
+            let rowSiteName = $(this).find("td").eq(0).text();
+            console.log(rowSiteName);
+        });
+    */
 
     // bind table row button click
     projectTable.on('click', 'button', function (e) {
@@ -64,7 +60,9 @@ let projectMain = function (){
         let href = `${hrefDomain}${urlPath}/${data[0]}.html?${hrefParams}`;
         window.location=href;
     });
+}
 
+const bindDomainSelectorSlider = () => {
     let $domainSelector = $('#domainSelector');
     $domainSelector.on('click', () => {
         if ($domainSelector.hasClass('isOn')) {
@@ -79,15 +77,27 @@ let projectMain = function (){
 
         }
     });
+}
 
+const bindVisitorCopyToClipboardBtn = () => {
     let $copyVisitorIdBtn = $('#copyVisitorIdBtn');
     $copyVisitorIdBtn.on( "click", function() {
         copyToClipboard('visitorId');
     });
-};
+}
+
+const bindDomainWarningIcon = () => {
+    $('.fa-warning.isOff').on('click', () => {
+        alert('lpTag is not white listed for localhost');
+    });
+}
 
 
 $(function() {
-    projectMain();
+    hrefDomain = localDomain;
+    initDataTable();
+    bindDomainSelectorSlider();
+    bindVisitorCopyToClipboardBtn();
+    bindDomainWarningIcon();
 });
 
